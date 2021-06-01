665cefc82d7926c5f28457ef725ba6954c9c1f8f
Caia Chuang <caiachuang@gmail.com>
2021-06-01 13:12:30 -0700

    03-6 while loops in Java.

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Pick a number between 1 and 10");
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        boolean isOnRepeat = true;
 
-        int inputtedNum = scanner.nextInt();
+        while (isOnRepeat) {
+            System.out.println("Playing current song");
+            System.out.println("Would you like to take this song off of repeat? If so, answer yes");
+            String userInput = input.next();
 
-        if (inputtedNum < 5) {
-            System.out.println("Enjoy the good luck a friend brings you.");
-        } else {
-            System.out.println("Your shoe selection will make you happy today.");
-
+            if (userInput.equals("yes")) {
+                isOnRepeat = false;
+            }
         }
+
+        System.out.println("Playing next song");
     }
 }
