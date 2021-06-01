9ad8fbf6f563fb45bbd5319539621ef3dbac77cc
Caia Chuang <caiachuang@gmail.com>
2021-06-01 14:18:09 -0700

    5-4 parameters in Java.

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,14 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void announceDeveloperTeaTime() {
-        System.out.println("Waiting for developer tea time...");
-        System.out.println("Type in a random word and press Enter to start developer tea time.");
-        Scanner input = new Scanner(System.in);
-        input.next();
-        System.out.println("It's developer tea time!");
+    public static void calculateTotalMealPrice(double listedMealPrice, double tipRate, double taxRate) {
+        double tip = tipRate * listedMealPrice;
+        double tax = taxRate * listedMealPrice;
+        double result = listedMealPrice + tip + tax;
+        System.out.println("Your total meal price is " + result);
     }
+
     public static void main(String[] args) {
-        System.out.println("Before Call");
-
-        announceDeveloperTeaTime();
-
-        System.out.println("After Call");
-
-        announceDeveloperTeaTime();
-
-        System.out.println("After Call again");
+        calculateTotalMealPrice(45, .15, .12);
     }
 }
