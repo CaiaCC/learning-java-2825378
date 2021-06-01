21eca3c6936dfefa3f0dc5a5ead2ca77a63e95fa
Caia Chuang <caiachuang@gmail.com>
2021-06-01 14:05:26 -0700

    5-2 defining functions in Java.

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        String q = "3 + 1 * 2 = ?";
-        String choiceOne = "The answer is 8";
-        String choiceTwo = "The answer is 5";
-        String choiceThree = "I don't know";
 
-        int correctAnswer = 2;
-
-
-        System.out.println(q);
-        System.out.println("1) " + choiceOne);
-        System.out.println("2) " + choiceTwo);
-        System.out.println("3) " + choiceThree);
-        System.out.println("What is your answer?");
-
+    public static void announceDeveloperTeaTime() {
+        System.out.println("Waiting for developer tea time...");
+        System.out.println("Type in a random word and press Enter to start developer tea time");
         Scanner input = new Scanner(System.in);
-        
-        int userInput = input.nextInt();
-
-        if (userInput == correctAnswer) {
-            System.out.println("You are right!");
-        } else {
-            System.out.println("You are wrong! The correct answer is 2!");
-        }
+        input.next();
+        System.out.println("It's developer tea time!");
+    }
+    public static void main(String[] args) {
 
     }
 }
