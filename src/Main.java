74634bfcb3ed6ddf8bb9ec0bcd453276c5163c55
Caia Chuang <caiachuang@gmail.com>
2021-06-01 12:55:22 -0700

    Clean up.

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Primitive types
-//        int studentAge = 15;
+
         double studentGPA = 3.54;
-//        boolean hasPerfectAttendance = true;
-//        String studentName = "Caia Chuang";
+
         String studentFirstName = "Caia";
         String studentLastName = "Chuang";
-//        char studentFirstInitial = studentFirstName.charAt(0);
-//        char studentLastInitial = studentLastName.charAt(0);
-
-//        System.out.println(studentAge);
-//        System.out.println(studentGPA);
-//        System.out.println(hasPerfectAttendance);
-//        System.out.println(studentFirstName);
-//        System.out.println(studentLastName);
-//        System.out.println(studentFirstInitial);
-//        System.out.println(studentLastInitial);
 
         System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
         System.out.println("What do you want to update it to?");
